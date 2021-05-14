9ad2836851163074621eb53db3c44b8588ffeb33
Niclas Lindstedt <nicl@slindstedt.se>
2021-05-14 14:54:43 +0200

    Add support for output dirs in unpack functions

--- lib/common/archiving.sh
+++ lib/common/archiving.sh
@@ -40,23 +40,19 @@ unpack() {
   log "Removing contents of $target_path"
   remove_file_recursive "${target_path%/}"/*
 
-  go "$target_path"
-
-    log "Unpacking archive at $2"
-    fileext="${filename_clean##*.}"
-    if [[ "$fileext" = "tgz" ]]; then
-      unpack_tar "$filename_clean" || return 1
-    elif [[ "$fileext" = "7z" ]]; then
-      unpack_7zip "$filename_clean" || return 1
-    elif [[ "$fileext" = "zip" ]]; then
-      unpack_zip "$filename_clean" || return 1
-    elif [[ "$fileext" = "rar" ]]; then
-      unpack_rar "$filename_clean" || return 1
-    else
-      error "Unknown file extension '$fileext'"
-    fi
-
-  back
+  log "Unpacking archive at $target_path"
+  fileext="${filename_clean##*.}"
+  if [[ "$fileext" = "tgz" ]]; then
+    unpack_tar "$filename_clean" "$target_path" || return 1
+  elif [[ "$fileext" = "7z" ]]; then
+    unpack_7zip "$filename_clean" "$target_path" || return 1
+  elif [[ "$fileext" = "zip" ]]; then
+    unpack_zip "$filename_clean" "$target_path" || return 1
+  elif [[ "$fileext" = "rar" ]]; then
+    unpack_rar "$filename_clean" "$target_path" || return 1
+  else
+    error "Unknown file extension '$fileext'"
+  fi
 }
 
 # Helper functions
@@ -64,7 +60,7 @@ pack_tar() { $(sudo_if_unwritable "$2") tar czfv "$1" "$2" 1>"$OUTPUT" || return
 pack_7zip() { $(sudo_if_unwritable "$2") 7zr a -r "$1" "$2" 1>"$OUTPUT" || return 1; }
 pack_zip() { $(sudo_if_unwritable "$2") zip -r "$1" "$2" 1>"$OUTPUT" || return 1; }
 pack_rar() { $(sudo_if_unwritable "$2") rar a -r "$1" "$2" 1>"$OUTPUT" || return 1; }
-unpack_tar() { $(sudo_if_unwritable .) tar xzfv "$1" 1>"$OUTPUT" || return 1; }
-unpack_7zip() { $(sudo_if_unwritable .) 7zr x "$1" 1>"$OUTPUT" || return 1; }
-unpack_zip() { $(sudo_if_unwritable .) unzip "$1" 1>"$OUTPUT" || return 1; }
-unpack_rar() { $(sudo_if_unwritable .) unrar x -r "$1" 1>"$OUTPUT" || return 1; }
+unpack_tar() { $(sudo_if_unwritable "$2") tar xzfv "$1" -C "$2" 1>"$OUTPUT" || return 1; }
+unpack_7zip() { $(sudo_if_unwritable "$2") 7zr x "$1" -o"$2" 1>"$OUTPUT" || return 1; }
+unpack_zip() { $(sudo_if_unwritable "$2") unzip "$1" -d "$2" 1>"$OUTPUT" || return 1; }
+unpack_rar() { $(sudo_if_unwritable "$2") unrar x -r "$1" "$2" 1>"$OUTPUT" || return 1; }
