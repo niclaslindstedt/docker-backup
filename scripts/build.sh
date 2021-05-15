b73961a088da01eb8d1caa52950cf409ce9ce3b1
Niclas Lindstedt <nicl@slindstedt.se>
2021-05-15 17:29:08 +0200

    Add continuous deploy

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,14 +3,18 @@
 # shellcheck disable=SC2048,SC2086
 
 main() {
+  local registry
+
+  registry="${1:-niclaslindstedt}"
+
   echo "Building latest-docker"
-  docker build --build-arg INSTALL_DOCKER=true -t "docker-backup:latest-docker" . || exit 1
+  docker build --build-arg INSTALL_DOCKER=true -t "$registry/docker-backup:latest-docker" . || exit 1
 
   echo "Building latest"
-  docker build --build-arg INSTALL_DOCKER=false -t "docker-backup:latest" . || exit 1
+  docker build --build-arg INSTALL_DOCKER=false -t "$registry/docker-backup:latest" . || exit 1
 }
 
 SCRIPTDIR=$(dirname "$(readlink -f "$0")")
 cd "$SCRIPTDIR/.." || exit 1
 
-main $*
+main $1
