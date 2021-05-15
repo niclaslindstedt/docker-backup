149294918acac31633efd143c8e46db499167b88
Niclas Lindstedt <nicl@slindstedt.se>
2021-05-15 17:17:26 +0200

    Add github workflow

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# shellcheck disable=SC2048,SC2086
+
+main() {
+  echo "Building latest-docker"
+  docker build --build-arg INSTALL_DOCKER=true -t "docker-backup:latest-docker" . || exit 1
+
+  echo "Building latest"
+  docker build --build-arg INSTALL_DOCKER=false -t "docker-backup:latest" . || exit 1
+}
+
 SCRIPTDIR=$(dirname "$(readlink -f "$0")")
 cd "$SCRIPTDIR/.." || exit 1
 
-version="${1:-latest}"
-echo "Building $version"
-
-docker build --build-arg INSTALL_DOCKER=true -t "docker-backup:$version-docker" .
-docker build --build-arg INSTALL_DOCKER=false -t "docker-backup:$version" .
+main $*
---
+++ scripts/tag.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# shellcheck disable=SC2048,SC2086
+
+main() {
+  local version registry_uri major minor image
+
+  [[ "$1" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]] || { echo "Version should be vX.Y.Z."; exit 1; }
+
+  version="$1"
+  registry_uri="${2:-niclaslindstedt}"
+  image="$registry_uri/docker-backup"
+  major="${version%.*}"
+  minor="${version%.*.*}"
+
+  echo "Tagging images ($version, $major, $minor)"
+
+  docker tag "docker-backup:latest-docker" "$image:$version-docker"
+  docker tag "docker-backup:latest-docker" "$image:$major-docker"
+  docker tag "docker-backup:latest-docker" "$image:$minor-docker"
+  docker tag "docker-backup:latest" "$image:$version"
+  docker tag "docker-backup:latest" "$image:$major"
+  docker tag "docker-backup:latest" "$image:$minor"
+}
+
+SCRIPTDIR=$(dirname "$(readlink -f "$0")")
+cd "$SCRIPTDIR/.." || exit 1
+
+main $*
